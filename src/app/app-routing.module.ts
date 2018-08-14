1782c6e052d576459e37b7adca66152f094b25a6
kaikas <max.riechelmann@googlemail.com>
2018-08-14 19:00:15 +0200

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlueprintsComponent} from './blueprints/blueprints.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BlueprintsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/blueprints/blueprints.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blueprints',
+  templateUrl: './blueprints.component.html',
+  styleUrls: ['./blueprints.component.scss']
+})
+export class BlueprintsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
