ffee8e9e2776ab044d64ac053ffd2e0a80133fc8
kaikas <max.riechelmann@googlemail.com>
2018-08-17 16:50:25 +0200

    assets and typeIDs dataservice

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,4 +11,7 @@ export class DataService {
   getBlueprints() {
     return this.http.get('https://esi.tech.ccp.is/latest/markets/10000002/orders/?datasource=tranquility&order_type=sell&type_id=16670')
   }
+  getTypeIDs() {
+    return this.http.get('http://sw-gaming.org/eve/assets/sde/typeIDs.txt')
+  }
 }
